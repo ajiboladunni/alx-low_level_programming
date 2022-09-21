1cf93e57174eefa303c6aef914d1a28d95aee4b8
ajiboladunni <alayazainabajibola@gmail.com>
2022-09-21 14:09:34 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - magic
- *
- * Return: Always 0
- */
-
 int main(void)
 {
 	int n;
@@ -20,12 +14,10 @@ int main(void)
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
-	 * - you are not allowed to use ,
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	*(p + 5) = 98;
+	(p[5] = 98);
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
-	printf("a[2] = 98\n");
 	return (0);
 }
